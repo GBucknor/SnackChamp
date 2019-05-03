75cdc5a6ccb708ee92c30b20fcd6adc8104ac095
GarelB <gbucknor@tractionondemand.com>
2019-05-03 16:42:56 -0700

    Made some improvements, fixed the time interval bug.

--- js/chooser.js
+++ js/chooser.js
@@ -1,3 +1,9 @@
+/**
+ * Picks 2 people from your team to bring in snacks next week.
+ * @author Garel Bucknor
+ */
+
+/* VARIABLES */
 const names = [
     'David Barrera',
     'Madeline Luke',
@@ -16,6 +22,9 @@ const names = [
     'Brian Anderson'
 ];
 let defaultDay = 0;
+let currentIndex = 0;
+
+/* FUNCTIONS */
 const getTextDay = (dayNumber) => {
     let days = [
         'Monday',
@@ -26,25 +35,41 @@ const getTextDay = (dayNumber) => {
 
 const rollForChamp = () => {
     let numberOfOptions = names.length;
+    currentIndex = Math.floor(Math.random() * Math.floor(numberOfOptions));
     $('#roll-text').html(
-        names[Math.floor(Math.random() * Math.floor(numberOfOptions))]
+        names[currentIndex]
     );
+}
+
+const toggleBtn = (btn) => {
+    if (btn.attr('disabled')) {
+        btn.attr('disabled', false);
+    } else {
+        btn.attr('disabled', true);
+    }
+}
+
+const populateTractioniteTable = () => {
+    let row = $('<tr></tr>');
+    let day = $('<td></td>').html(getTextDay(defaultDay));
+    let name = $('<td></td>').html($('#roll-text').html());
+    $(row).append(day);
+    $(row).append(name);
+    $('#champs').append(row);
+    names.pop(currentIndex);
+    (++defaultDay <= 1) ? toggleBtn($('#btn-circle')) : $('#roll-text').html('Enjoy your snacks!');
+    (defaultDay > 0) ? $('#undo-btn').attr('hidden', false) : null;
 }
 
 const deceleratingTimeout = (callback, factor, times) => {
     let internalCallback = function(tick, counter) {
+        toggleBtn($('#btn-circle'));
         return () => {
             if (--tick >= 0) {
                 window.setTimeout(internalCallback, ++counter * factor);
                 callback();
             } else {
-                let row = $('<tr></tr>');
-                let day = $('<td></td>').html(getTextDay(defaultDay));
-                let name = $('<td></td>').html($('#roll-text').html());
-                $(row).append(day);
-                $(row).append(name);
-                $('#champs').append(row);
-                (++defaultDay > 1) ? $('#btn-circle').attr('disabled', true) : null;
+                populateTractioniteTable();
             }
         }
     } (times, 0);
